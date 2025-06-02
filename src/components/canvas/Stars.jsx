f2436002e5799ebd6bee6ac24b296fa1b3de4939
SayakBose1 <150025010+SayakBose1@users.noreply.github.com>
2025-06-02 20:06:46 +0530

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,12 +1,3 @@
-import React from 'react'
-
-const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
-
-export default Stars
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
